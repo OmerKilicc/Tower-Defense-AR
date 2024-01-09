bd73b6d6927658e651527d3a827f64194fdcfd76
Omer Faruk Kilic <omeerfkiliic@gmail.com>
2024-01-09 18:41:12 +0300

    made simple (shitty) pathfindig for enemy

--- Assets/_Scripts/Enemy/EnemyMovement.cs
+++ Assets/_Scripts/Enemy/EnemyMovement.cs
@@ -12,37 +12,56 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] List<Waypoint> tiles = new List<Waypoint>();
-    [SerializeField] [Range(0f,5f)] float speed = 1f;
+	[SerializeField] List<Waypoint> path = new List<Waypoint>();
+	[SerializeField][Range(0f, 5f)] float speed = 1f;
 
-    void Start()
-    {
-        StartCoroutine(FollowPath());
-        
-    }
+	void Start()
+	{
+		FindPathBruteForce();
+		ReturnToStart();
+		StartCoroutine(FollowPath());
+	}
 
-    IEnumerator FollowPath()
-    {
-        //iterating on all the waypoint
-        foreach (var waypoint in tiles)
-        {
+	private void FindPathBruteForce()
+	{
+		path.Clear();
+
+		GameObject parent = GameObject.FindGameObjectWithTag("Path");
+		
+		foreach (Transform child in parent.transform)
+		{
+			path.Add(child.GetComponent<Waypoint>());
+		}
+	}
+
+	private void ReturnToStart()
+	{
+		transform.position = path[0].transform.position;
+	}
+
+	IEnumerator FollowPath()
+	{
+		//iterating on all the waypoint
+		foreach (var waypoint in path)
+		{
 
 			Vector3 startPosition = transform.position;
-            Vector3 endPosition = waypoint.transform.position;
-            float distanceCovered = 0f;
+			Vector3 endPosition = waypoint.transform.position;
+			float distanceCovered = 0f;
 
-            //making it look at the direction which its moving
-            transform.LookAt(endPosition);
+			//making it look at the direction which its moving
+			transform.LookAt(endPosition);
 
-            while (distanceCovered < 1f)
-            {
-                distanceCovered += Time.deltaTime * speed;
-                transform.position = Vector3.Lerp(startPosition, endPosition, distanceCovered);
+			while (distanceCovered < 1f)
+			{
+				distanceCovered += Time.deltaTime * speed;
+				transform.position = Vector3.Lerp(startPosition, endPosition, distanceCovered);
 				yield return new WaitForEndOfFrameUnit();
 			}
+		}
 
-        }
-    }
-
-
+		//When path is finished
+		Destroy(gameObject);
+		//TODO: damage the player
+	}
 }
