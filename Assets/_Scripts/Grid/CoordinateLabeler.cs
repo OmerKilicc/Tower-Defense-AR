26b061cc4aae93712fed7397946a0a0ffd7d595e
Omer Faruk Kilic <omeerfkiliic@gmail.com>
2024-01-09 17:18:30 +0300

    Created Debug Tool For Labels

--- Assets/_Scripts/Grid/CoordinateLabeler.cs
+++ Assets/_Scripts/Grid/CoordinateLabeler.cs
@@ -1,40 +1,69 @@
+using System.Reflection.Emit;
 using TMPro;
 using UnityEngine;
 
 [ExecuteAlways]
 public class CoordinateLabeler : MonoBehaviour
 {
-    TextMeshPro _gridLabel;
-    Vector2Int _coordinates = new Vector2Int();
+	[SerializeField] Color defaultLabelColor = Color.white;
+	[SerializeField] Color blockedLabelColor = Color.red;
 
-    void Awake()
-    {
-        _gridLabel = GetComponent<TextMeshPro>();
-    }
+	TextMeshPro _gridLabel;
+	Vector2Int _coordinates = new Vector2Int();
+	Waypoint _waypoint;
 
-    void Update()
-    {
-        if (!Application.isPlaying)
-        {
-            DisplayCoordinates();
-        }
-    }
+	void Awake()
+	{
+		_gridLabel = GetComponent<TextMeshPro>();
+		_gridLabel.enabled = false;
 
-    void DisplayCoordinates() 
-    {
-        var snapSettingsX = UnityEditor.EditorSnapSettings.move.x;
-        var snapSettingsY = UnityEditor.EditorSnapSettings.move.z;
+		_waypoint = GetComponentInParent<Waypoint>();
+		DisplayCoordinates();
+	}
+
+	void Update()
+	{
+		if (!Application.isPlaying)
+		{
+			DisplayCoordinates();
+			UpdateObjectName();
+		}
+
+		ColorCoordinates();
+		ToggleLabels();
+	}
+
+	//Debug tool to hide and make visible the labels
+	void ToggleLabels() 
+	{
+		if(Input.GetKeyDown(KeyCode.C)) 
+		{
+			_gridLabel.enabled = !_gridLabel.IsActive();
+		}
+	}
+	void ColorCoordinates()
+	{
+		if (_waypoint.IsPlacable)
+			_gridLabel.color = defaultLabelColor;
+		else
+			_gridLabel.color = blockedLabelColor;
+	}
+
+	void DisplayCoordinates()
+	{
+		var snapSettingsX = UnityEditor.EditorSnapSettings.move.x;
+		var snapSettingsY = UnityEditor.EditorSnapSettings.move.z;
 
 		_coordinates.x = Mathf.RoundToInt(transform.parent.position.x / snapSettingsX);
-        _coordinates.y = Mathf.RoundToInt(transform.parent.position.z / snapSettingsY);
+		_coordinates.y = Mathf.RoundToInt(transform.parent.position.z / snapSettingsY);
 
-        _gridLabel.text = $"{_coordinates.x},{_coordinates.y}";
+		_gridLabel.text = $"{_coordinates.x},{_coordinates.y}";
 
-        UpdateObjectName();
-    }
+		UpdateObjectName();
+	}
 
-    void UpdateObjectName()
-    {
-        transform.parent.name = _coordinates.ToString();
-    }
+	void UpdateObjectName()
+	{
+		transform.parent.name = _coordinates.ToString();
+	}
 }
